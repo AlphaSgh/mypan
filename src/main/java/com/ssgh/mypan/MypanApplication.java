55b978d514fc3f82a3da8b8d8ca00fd4c3200326
AlphaSgh <upupsgh@163.com>
2023-04-07 17:23:11 +0800

    mypan 20230407

---
+++ src/main/java/com/ssgh/mypan/MypanApplication.java
@@ -0,0 +1,13 @@
+package com.ssgh.mypan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssgh/mypan/MypanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssgh.mypan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
